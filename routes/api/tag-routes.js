1bab5093ca435853d064ffc88d9ecdd5dc32e1cd
AMiro <aj.miro.b@gmail.com>
2023-04-23 20:23:33 -0500

    Add POST route for tag-routes.js (3/5)

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,8 +31,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post("/", async (req, res) => {
+  try {
+    const newTagData = await Tag.create(req.body);
+    res.status(200).json(newTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
