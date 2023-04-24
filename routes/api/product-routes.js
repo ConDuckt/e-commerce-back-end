735c7c45f77edad15c662c736cc2327b647f8264
AMiro <aj.miro.b@gmail.com>
2023-04-23 19:41:13 -0500

    Add DELETE route to product-routes.js (3/5)

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -103,8 +103,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedProductData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedProductData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deletedProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
